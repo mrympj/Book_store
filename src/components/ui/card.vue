ae647a0c99bb2d8c4c3111a3ee5bf2ed001f75fb
unknown <mrympj>
2022-02-07 15:49:22 +0330

    Add author list to previous design

--- src/components/ui/card.vue
+++ src/components/ui/card.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="col-3 m-1 ">
       <router-link :to="'/detail/' + bookList.id" style="text-decoration: none;">
-        <b-card :img-src="bookList.image" img-alt="Card image" img-height="170rem" img-left class="mb-3 p-4 shadow-lg border-0 card"
-         style="max-height:20rem; min-height:9rem; min-width:14 rem; color:#818181; background-image: linear-gradient( #ffdde1, #ee9ca7);">
-          <b-card-text style="font-size: 0.9rem">
-            {{bookList.name}}<br>
-            by {{bookList.author}}<br><br>
+        <b-card :img-src="bookList.image" img-alt="Card image" img-height="170rem" img-left class="mb-3 p-2 shadow-lg border-0 card"
+         style="max-height:20rem; min-height:9rem; min-width:14 rem;  color: white; background-image: linear-gradient( #ffdde1, #ee9ca7);">
+          <b-card-text>
+            <p style="font-size: 1rem"><b>{{bookList.name}}</b><br></p>
+            <p style="font-size: 0.8rem">by <b>{{bookList.author}}</b><br><br></p>
           </b-card-text>
         </b-card>
       </router-link>
